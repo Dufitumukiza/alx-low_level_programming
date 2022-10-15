2fc1b59115b478511db0888451b6728fada03a4b
Dufitumukiza <dufijonathan@gmail.com>
2022-10-15 14:56:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,8 @@
  * Auth: Brennan D Baraban
  */
 
+#include <unistd.h>
+
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to standard error.
