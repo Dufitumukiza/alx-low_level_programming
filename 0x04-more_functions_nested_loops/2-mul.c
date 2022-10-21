ab3cdd66b97fd185f07568d84e170ff3aca02de6
Dufitumukiza <dufijonathan@gmail.com>
2022-10-21 00:09:27 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mut - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: the multiplication of a and b
+ */
+int mut(int a, int b)
+{
+	return (a * b);
+}
