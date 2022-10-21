f97f18188324d78192e05e224765990b41a750cc
Dufitumukiza <dufijonathan@gmail.com>
2022-10-21 00:12:29 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * mut - multiply two integers
+ * mul - multiply two integers
  * @a: first int
  * @b: second int
  * Return: the multiplication of a and b
  */
-int mut(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
