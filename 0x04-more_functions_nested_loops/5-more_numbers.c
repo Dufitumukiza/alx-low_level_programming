f048353e18bdb485d973d8466d032b6ee1dfe1db
Dufitumukiza <dufijonathan@gmail.com>
2022-10-21 08:52:21 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,20 @@
 
 /**
  * more_numbers - prints 0 to 14 ten times
- * Return: Always 0
+ * Return: no return
  */
 void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 1; a <= 10; a++)
 	{
 		for (num = 0; n <= 14; n++)
 		{
-			if (num > 9)
+			if (num >= 10)
 			{
-				_putchar((num / 10) + '0');
-				_putchar((num % 10) + '0');
+				_putchar('1');
+				_putchar(num % 10 + '0');
 			}
 		}
 		_putchar('\n');
