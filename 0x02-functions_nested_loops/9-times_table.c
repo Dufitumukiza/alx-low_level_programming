12c60393e69ae33b984eafc7a76c0fc7ef9037c6
Dufitumukiza <dufijonathan@gmail.com>
2022-10-24 23:38:15 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,13 +27,16 @@ void times_table(void)
 				_putchar(u + '0');
 			}
 			else
+			{
 				if (y != 0)
 				{
 					_putchar(44);
 					_putchar(32);
+					_putchar(32);
 				}
 
-			_putchar(z + '0');
+				_putchar(z + '0');
+			}
 
 		}
 
