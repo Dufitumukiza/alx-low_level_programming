3584bb3b58e83ad4028cf6934afcac598bc7fbdd
Dufitumukiza <dufijonathan@gmail.com>
2022-11-28 02:41:34 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
-#include "function_ointers.h"
+#include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
+ * print_name - a function that prints a name.i
+ * @name: the name given
+ * @f: function of name
  *
  * Return: void
  */
